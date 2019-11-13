461b7bca4f28ba5a3871d25638a56245284dd0c7
Sven Garson <garson_sven@hotmail.com>
2019-11-13 18:09:19 +0100

    Adding RB120 21 assignment files

--- RB120/Lesson5_Slightly-Larger-OO-Programs/Ex8_OO-Twenty-One/oo_21_rubocop.rb
+++ RB120/Lesson5_Slightly-Larger-OO-Programs/Ex8_OO-Twenty-One/oo_21_rubocop.rb
@@ -1,3 +1,8 @@
+class GameConst
+  WINNING_HAND_VALUE = 21
+  DEALER_MIN_HAND_VALUE = 17
+end
+
 class Card
   
   attr_accessor :suit, :type, :value
@@ -8,8 +13,44 @@ class Card
     self.value = value
   end
 
+  def ascii_art_lines(show_face: true)
+
+    # determine what string to use for card type
+    face_card_names = %w(Ace King Queen Jack)
+    show_type_str = case face_card_names.include?(type)
+                  when true
+                    expand_str(type.chr, 2)
+                  else
+                    expand_str(value.to_s, 2)
+                  end
+
+    ascii_strarr = [
+      ' _____ ',
+      "|#   |".sub('#', show_type_str),
+      '|     |',
+      '|     |',
+      '|____#|'.sub('#', suit.chr)
+    ]
+
+    unless show_face
+      ascii_strarr.map! do |ascii_line|
+        ascii_line.gsub(/[^_ |]/, '#')
+      end
+    end
+
+    ascii_strarr
+  end
+
   def to_s
     "#{type} of #{suit}"
+  end
+
+  def expand_str(str, desired_length)
+    if str.length < desired_length
+      str.ljust(desired_length)
+    else
+      str
+    end
   end
 end
 
@@ -35,6 +76,7 @@ class Deck
 
   def shuffle
     3.times { |_| cards.shuffle! }
+    self
   end
 
   def pop_card
@@ -43,6 +85,10 @@ class Deck
 end
 
 class Participant
+  include Comparable
+
+  CHOICE_STAY = :stay
+  CHOICE_HIT = :hit
 
   attr_accessor :cards, :busted, :hand_value
 
@@ -52,26 +98,72 @@ class Participant
     self.hand_value = 0
   end
 
-  def hit(dealer)
-    dealer.deal(self)
+  def choose_until_stay_or_bust(game, dealer)
+    loop do
+      choice = choose_stay_or_hit
+
+      # participant is done when stays
+      break if choice == CHOICE_STAY
+
+      if choice == CHOICE_HIT
+        dealer.deal(self)
+        self.busted = true if hand_value > GameConst::WINNING_HAND_VALUE
+      end
+
+      game.show_cards
+
+      # participant is done when bust
+      break if bust?
+    end
+  end
+
+  # to be overriden by dealer and player
+  def choose_stay_or_hit; end
+
+  def compute_hand_value
+    ace_count = cards.select { |card| card.type == 'Ace' }.size
+    non_aces = cards.reject { |card| card.type == 'Ace' }
+    base_score = non_aces.reduce(0) { |accum, card| accum + card.value }
+    ace_rest = GameConst::WINNING_HAND_VALUE - base_score
+    final_score = base_score
+    ace_count.downto(0) do |full_aces|
+      test_score = (full_aces * 11) + (ace_count - full_aces)
+      final_score = base_score + test_score
+      break if final_score <= GameConst::WINNING_HAND_VALUE
+    end
+    self.hand_value = final_score
   end
 
   def take_card(card)
     cards << card
+    compute_hand_value
+  end
+  
+  def bust?
+    busted
   end
 
-  def print_cards(show_all: true)
-    cards.each_with_index do |card, index|
-      c_index = index + 1
-      if show_all
-        puts "#{c_index}) " + card.to_s
-      else
-        if c_index > 1
-          puts "#{c_index}) " + "################"
-        else
-          puts "#{c_index}) " + card.to_s
-        end
+  def <=>(other)
+    hand_value <=> other.hand_value
+  end
+
+  def print_cards(show_all: true, padding: 2)
+    return if cards.empty?
+    
+    show_card_faces = :true
+    cards_ascii_lines = cards.map do |card|
+      lines = card.ascii_art_lines(show_face: show_card_faces)
+      show_card_faces = false unless show_all
+      lines
+    end
+
+    # to print cards horizontally we need to print cards row by row
+    rows_to_print = cards_ascii_lines.first.length
+      rows_to_print.times do |row_index|
+        cards_ascii_lines.each do |ascii_line|
+        print "#{ascii_line[row_index]}" + (' ' * padding)
       end
+      print "\n"
     end
   end
 end
@@ -86,6 +178,21 @@ class Dealer < Participant
     deck.shuffle
   end
 
+  def choose_until_stay_or_bust(game, dealer=self)
+    super
+  end
+
+  def choose_stay_or_hit
+    Utils.prompt_dotted_wait("\nDealer is thinking ", 3)
+
+    # hit until hand value at least 17
+    if hand_value < GameConst::DEALER_MIN_HAND_VALUE
+      CHOICE_HIT
+    else
+      CHOICE_STAY
+    end
+  end
+
   def deal(receiver, card_count=1)
     card_count.times { |_| receiver.take_card(deck.pop_card) }
   end
@@ -93,7 +200,13 @@ end
 
 class Player < Participant
 
-
+  def choose_stay_or_hit
+    # prompt for choice
+    valid_choice_str = Utils.prompt_choice_hit_or_stay
+    
+    # return choice as option
+    valid_choice_str.start_with?('h') ? CHOICE_HIT : CHOICE_STAY
+  end
 end
 
 class Game21
@@ -108,28 +221,53 @@ class Game21
   end
 
   def start
+
     display_welcome
     display_rules
     
     deal_initial_cards
     show_cards
+   
+    player.choose_until_stay_or_bust(self, dealer)
 
-    
-    player.turn
-    # player busted?
+    unless player.bust?
+      dealer.choose_until_stay_or_bust(self)
+    end
 
-    dealer.turn
-    # dealer busted?
+    show_cards(reveal_dealer: true)
 
-    #determine_winner
-
-    #display_winner
+    determine_winner
+    display_winner
     
     display_goodbye
   end
 
-  def game_over?
-    false
+  def display_winner
+    winner_msg = if winner == player
+                   "\nPlayer wins the game!"
+                 elsif winner == dealer
+                   "\nDealer wins the game!"
+                 else
+                   "\nIt's a tie!"
+                 end
+    
+    puts winner_msg
+
+    print "\n(Press enter to stop the game)"
+    Utils.wait_for_any_input
+  end
+
+  def determine_winner
+    # winner set to nil in case of tie
+    self.winner = if player.bust?
+                    dealer
+                  elsif dealer.bust?
+                    player
+                  elsif player < dealer
+                    dealer
+                  elsif dealer < player
+                    player
+                  end
   end
 
   def deal_initial_cards
@@ -137,29 +275,28 @@ class Game21
     dealer.deal(dealer, 2)
   end
 
-  def clear
+  def show_cards(reveal_dealer: false)
     Utils.clear
-  end
 
-  def show_cards
-    # first of the dealers cards
-    puts "Dealer cards"
-    puts "------------"
-    dealer.print_cards(show_all: false)
-    # both of the player cards
-    puts "\nPlayer cards"
-    puts "------------"
+    # first of the dealer cards
+    puts "\n+--------------+"
+    puts "| Dealer cards | #{dealer.hand_value}"
+    puts "+--------------+"
+    dealer.print_cards(show_all: reveal_dealer)
+
+    # all the player cards
+    puts "\n+--------------+"
+    puts "| Player cards | #{player.hand_value}"
+    puts "+--------------+"
     player.print_cards
   end
 
   def display_goodbye
-    clear
-
-    puts "\nThank you for playing 21!\n\n"
+    puts "\nWe hope you enjoyed our take on 21 !\n\n"
   end
 
   def display_rules
-    clear
+    Utils.clear
     puts "Rules"
     puts "-----"
     puts "21 is about getting to a hand value as close to 21 as possible."
@@ -169,16 +306,29 @@ class Game21
     puts "\nIn case nobody is bust, the player with the highest hand value wins."
     puts "\nHand values"
     puts "-------------"
-    puts "Cards 2 through 10 are worth their face value in points."
+    puts "\nCards 2 through 10 are worth their face value in points."
     puts "Cards King(K), Queen(Q) and Jack(J) are worth 10 points."
-    puts "Aces are worth either 1 or 11, whichever brings you closest to 21."
+    puts "Aces are worth either 1 or 11, whichever brings you closest to 21.\n"
+
+    example_ascii_lines = Deck.new.shuffle.pop_card.ascii_art_lines
+    example_ascii_lines.each_with_index do |card_line, index|
+      rindex = index + 1
+
+      print card_line
+      if rindex == 2
+        print ' --> Card type (Ace, Jack, Ten(10), Two(2), ...)'
+      elsif rindex == 5
+        print ' --> Card suit (Diamonds, Hearts, Spades, ...)'
+      end
+      print "\n"
+    end
     
-    print "\n(Press enter to continue)"
+    print "\n\n(Press enter to continue)"
     Utils.wait_for_any_input
   end
 
   def display_welcome
-    clear
+    Utils.clear
     puts "\nWelcome to 21!"
 
     print "\n(Press enter to continue)"
@@ -195,6 +345,25 @@ class Utils
     loop do
       input = gets.chomp
       break
+    end
+  end
+
+  def self.prompt_dotted_wait(prompt_msg, wait_time_sec)
+    print prompt_msg
+    frac_wait_time = wait_time_sec.fdiv(3.0)
+    3.times do |_|
+      print '.'
+      sleep frac_wait_time
+    end
+  end
+
+  def self.prompt_choice_hit_or_stay
+    loop do
+      print "\nHit or stay? "
+      choice = gets.chomp
+      choice.downcase!
+      break(choice) if %w(hit stay h s).include?(choice)
+      print "\nChoose either hit(h) or stay(s)."
     end
   end
 end
