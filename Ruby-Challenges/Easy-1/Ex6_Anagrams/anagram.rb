90abad9138da8d55c0ce14c4343674e194880c61
Sven Garson <garson_sven@hotmail.com>
2020-01-10 12:37:07 +0100

    Fixing Ruby-Challenges source file indentation again

--- Ruby-Challenges/Easy-1/Ex6_Anagrams/anagram.rb
+++ Ruby-Challenges/Easy-1/Ex6_Anagrams/anagram.rb
@@ -1,28 +1,28 @@
 class Anagram
 
-	def initialize(reference_anagram)
-		self.reference_anagram = reference_anagram
-	end
+  def initialize(reference_anagram)
+    self.reference_anagram = reference_anagram
+  end
 
-	def match(potential_anagrams)
-		reference_lowcase = reference_anagram.downcase
+  def match(potential_anagrams)
+    reference_lowcase = reference_anagram.downcase
 
-		potential_anagrams.select do |potential|
-			potential_lowcase = potential.downcase
+    potential_anagrams.select do |potential|
+      potential_lowcase = potential.downcase
 
-			not_identical = (potential_lowcase != reference_lowcase)
-			is_anagram = identical_characters?(potential_lowcase, reference_lowcase)
+      not_identical = (potential_lowcase != reference_lowcase)
+      is_anagram = identical_characters?(potential_lowcase, reference_lowcase)
 
-			not_identical &&
-		end
-	end
+      not_identical &&
+    end
+  end
 
-	private
+  private
 
-	def identical_characters?(string_a, string_b)
-		string_a.chars.sort == string_b.chars.sort
-	end
+  def identical_characters?(string_a, string_b)
+    string_a.chars.sort == string_b.chars.sort
+  end
 
-	attr_accessor :reference_anagram
+  attr_accessor :reference_anagram
 
 end
